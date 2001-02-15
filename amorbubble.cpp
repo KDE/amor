36237d7877e6f8531ed56092ad8a3d0b55ed2189
Stephan Kulow <coolo@kde.org>
2001-02-15 09:39:50 +0000

    now I see. i18n(message.latin1()) is a duplicate i18n and breaks non-latin1
    svn path=/trunk/kdetoys/amor/; revision=82929

--- amorbubble.cpp
+++ amorbubble.cpp
@@ -66,7 +66,7 @@ AmorBubble::~AmorBubble()
 //
 void AmorBubble::setMessage(const QString& message)
 {
-    mMessage = QString( "<html>%1</html>" ).arg( i18n( message.utf8().data() ) );
+    mMessage = QString( "<html>%1</html>" ).arg( message );
     // hacks because heightForWidth() doesn't work.
     setGeometry( -1000, 0, 300, 1000 );
     show();
